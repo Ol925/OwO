c4cf7f1c2618e3f895b27e15b3a057f932ba0eb2
OL925 <xj1774881383@outlook.com>
2024-06-24 15:35:29 +0800

    README

--- src/main/java/com/OL925/OwOTesTMod/ClientProxy.java
+++ src/main/java/com/OL925/OwOTesTMod/ClientProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.OL925.OwOTesTMod;
 
 public class ClientProxy extends CommonProxy {
 
--- src/main/java/com/OL925/OwOTesTMod/OwOTesTMod.java
+++ src/main/java/com/OL925/OwOTesTMod/OwOTesTMod.java
@@ -1,5 +1,6 @@
-package com.myname.mymodid;
+package com.OL925.OwOTesTMod;
 
+import com.myname.mymodid.Tags;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,13 +11,13 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "MyMod", acceptedMinecraftVersions = "[1.7.10]")
-public class MyMod {
+@Mod(modid = OwOTesTMod.MODID, version = Tags.VERSION, name = "OwOTesTMod", acceptedMinecraftVersions = "[1.7.10]")
+public class OwOTesTMod {
 
-    public static final String MODID = "mymodid";
+    public static final String MODID = "OwOTesTMod";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
-    @SidedProxy(clientSide = "com.myname.mymodid.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
+    @SidedProxy(clientSide = "com.OL925.OwOTesTMod.ClientProxy", serverSide = "com.OL925.OwOTesTMod.CommonProxy")
     public static CommonProxy proxy;
 
     @Mod.EventHandler
